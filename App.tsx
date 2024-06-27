bd777733fa21604f03b51cf86a861928a9b8d630
Pedro Samuel <pedro.samuel@onesolutions.com.br>
2024-06-27 12:36:36 -0300

    chore: Add StatusBar component to adjust the top navigation bar color

--- App.tsx
+++ App.tsx
@@ -1,5 +1,15 @@
+import { StatusBar } from "react-native";
 import Home from "./src/screens/home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar // ela ajusta a barra de navegação superior do celular (barStyle mudar a cor, background pra definiar a cor dos icones)
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      ></StatusBar>
+      <Home />
+    </>
+  );
 }
